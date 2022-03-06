d7c906d8e3d0167b32fe960dc1fc7960fc467bb1
S1L3nT <munibesen@gmail.com>
2022-03-06 19:59:02 -0300

    proper fix
    srry for spam

--- src/simulation/elements/HCL.cpp
+++ src/simulation/elements/HCL.cpp
@@ -234,7 +234,7 @@ static int update(UPDATE_FUNC_ARGS) {
 
 
 
-			if (!sim->elements[rt].Properties & PROP_WATER && rt != PT_CLNE && rt != PT_PCLN && rt != PT_ACID && rt != PT_HCL && rt != PT_CAUS && RNG::Ref().chance(1, 8))
+			if (!(sim->elements[rt].Properties & PROP_WATER) && rt != PT_CLNE && rt != PT_PCLN && rt != PT_ACID && rt != PT_HCL && rt != PT_CAUS && RNG::Ref().chance(1, 8))
 			{
 				capacity = parts[i].tmp4 + parts[i].oxygens + parts[i].carbons + parts[i].hydrogens + parts[i].water + parts[i].nitrogens;
 				if (RNG::Ref().chance(1, restrict_flt(sim->elements[rt2].Hardness + parts[i].water - parts[i].temp, 1, MAX_TEMP)) && (parts[i].ctype == rt || parts[i].ctype == 0) && capacity < parts[i].tmpcity[7]) {
@@ -245,7 +245,7 @@ static int update(UPDATE_FUNC_ARGS) {
 							rt = parts[ID(r)].ctype;
 
 
-						if (((sim->elements[rt].Properties & PROP_EDIBLE || sim->elements[rt].Properties & PROP_ORGANISM || sim->elements[rt].Properties & PROP_ANIMAL || (parts[i].tmp4 + parts[i].oxygens + parts[i].carbons + parts[i].hydrogens + parts[i].water + parts[i].nitrogens != 0)) && rt != PT_STMH) && RNG::Ref().chance(1, 8))
+						if (((sim->elements[rt].Properties & PROP_EDIBLE || sim->elements[rt].Properties & PROP_ORGANISM || sim->elements[rt].Properties & PROP_ANIMAL || sim->elements[rt].Properties & TYPE_LIQUID || (parts[i].tmp4 + parts[i].oxygens + parts[i].carbons + parts[i].hydrogens + parts[i].water + parts[i].nitrogens != 0)) && rt != PT_STMH) && RNG::Ref().chance(1, 8))
 						{
 							if ((parts[ID(r)].hydrogens > 0 || parts[ID(r)].oxygens > 0 || parts[ID(r)].carbons > 0 || parts[ID(r)].nitrogens > 0 || parts[ID(r)].water > 0))// (rt == PT_FLSH || (rt == PT_BRKN && parts[ID(r)].ctype == PT_FLSH) )
 							{
